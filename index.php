23769de7b4f7eb8b1c71c0525c93ccefc7412785
김진솔 <97jinsol@naver.com>
2021-11-12 06:08:51 +0900

    처음 init 입니다

---
+++ index.php
@@ -0,0 +1,153 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Portfolio_Jinsol</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+
+	<link rel="preconnect" href="https://fonts.gstatic.com"> 
+	<link href="https://fonts.googleapis.com/css2?family=Black+And+White+Picture&family=Black+Han+Sans&family=Cute+Font&family=Do+Hyeon&family=Dokdo&family=East+Sea+Dokdo&family=Gaegu&family=Gamja+Flower&family=Gothic+A1&family=Gugi&family=Hi+Melody&family=Jua&family=Kirang+Haerang&family=Nanum+Brush+Script&family=Nanum+Gothic&family=Nanum+Gothic+Coding&family=Nanum+Myeongjo&family=Nanum+Pen+Script&family=Noto+Sans+KR&family=Noto+Serif+KR&family=Poor+Story&family=Single+Day&family=Song+Myung&family=Stylish&family=Sunflower:wght@300&family=Yeon+Sung&display=swap" rel="stylesheet">
+
+	<body class="landing is-preload">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					
+					<nav id="nav">
+						<ul>
+							<li><a href="index.html">Home</a></li>
+							
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>김진솔</h2>
+					<p style="font-family: 'Jua', sans-serif;">코딩이 너무 재미있는 신입 개발자입니다.
+						<br>
+					의사소통 능력, 효율 있는 성실함이 저의 장점입니다.</p>
+
+					
+					
+					
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>About me</h2>
+	
+							<p style="font-family: 'Jua', sans-serif;">어릴때부터 수학과 과학에 관심이 많았고 문제를 해결해나가는 과정이 너무 재밌었습니다. 
+							
+								<br>성인이 되고 몇번의 진로를 바꾸는 등의 어려움이 있었지만 이제는 정말 꿈을 만난 것 같습니다. 
+								<br>성실하고 열정있는 최고의 기술자가 되겠습니다.</p>
+						</header>
+						<span class="image featured"><img src="images/주마등.png" alt="" /></span>
+					</section>
+
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<span class="icon solid major fa-bolt accent2"></span>
+								<h3>BackEnd</h3>
+								<p style="font-family: 'Jua', sans-serif;">#java #spring #oracle</p>
+							</section>
+							<section>
+								<span class="icon solid major fa-chart-area accent3"></span>
+								<h3>FrontEnd</h3>
+								<p style="font-family: 'Jua', sans-serif;">#html #css #javascript </p>							
+							</section>
+						</div>
+						<div class="features-row">
+							<section>
+								<span class="icon solid major fa-cloud accent4"></span>
+								<h3>ETC</h3>
+								<p style="font-family: 'Jua', sans-serif;">#git #python</p>							
+							</section>
+							<section>
+								<span class="icon solid major fa-lock accent5"></span>
+								<h3>Certificate</h3>
+								<p style="font-family: 'Jua', sans-serif;">#정보처리기사 #TOEIC_SPEAKING_LEVEL6</p>							
+							</section>
+						</div>
+					</section>
+
+					<div class="row">
+						<div class="col-6 col-12-narrower">
+
+							<section class="box special">
+								<span class="image featured"><img src="images/kostagram진솔.png" alt="" /></span>
+								<h3>KOSTAGRAM</h3>
+								<p style="font-family: 'Jua', sans-serif;">개발기간 : 2021.04.29 ~ 2021.05.14
+									<br>
+									학원 사람들과의 추억을 저장하며 공유하기 위해 만든 소셜 네트워킹 서비스
+									<br>
+									#java #javascript #ajax #mvc 
+								</p>
+								<ul class="actions special">
+									
+								</ul>
+							</section>
+
+						</div>
+						<div class="col-6 col-12-narrower">
+
+							<section class="box special">
+								<span class="image featured"><img src="images/코셰린.png" alt="" /></span>
+								<h3>KOCHELIN</h3>
+								<p style="font-family: 'Jua', sans-serif;"> 개발기간 : 2021.06.21 ~ 2021.07.19
+									<br>
+									보다 편리하고 효율적인 식당 예약 서비스. 인기 식당 등이 한눈에 파악 가능하다.
+									<br>
+									#java #spring #Mybatis  
+								</p>								<ul class="actions special">
+									
+								</ul>
+							</section>
+
+						</div>
+					</div>
+
+				</section>
+
+			<!-- CTA -->
+		
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+						<li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+						<li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+						<li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+						<li><a href="#" class="icon brands fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon brands fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+					<ul class="copyright">
+						<li>&copy; Untitled. All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/browser.min.js"></script>
+			<script src="assets/js/breakpoints.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
